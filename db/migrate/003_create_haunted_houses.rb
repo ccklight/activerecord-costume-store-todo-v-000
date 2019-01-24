c4cf723a5e20456c4a9bc6a670620b8b7a5b6574
Cheryl K <cherylkelson@icloud.com>
2019-01-24 00:46:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -24,7 +24,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-      
+
       t.timestamps null: false
     end
   end
